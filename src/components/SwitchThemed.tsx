c54c11e806374a5d20a71889a0659bb9dae920cb
kogekiplay <w981612327@gmail.com>
2023-05-25 15:22:18 +0800

    fix background color

--- src/components/SwitchThemed.tsx
+++ src/components/SwitchThemed.tsx
@@ -12,7 +12,7 @@ const Switch = ReactSwitch.default ? ReactSwitch.default : ReactSwitch;
 
 function SwitchThemed({ checked = false, onChange, theme, name }) {
   const offColor = theme === 'dark' ? '#393939' : '#e9e9e9';
-  const onColor = theme === 'dark' ? '#306081' : '#005caf';
+  const onColor = theme === 'dark' ? '#ffbccf' : '#fb8aaa';
   return (
     <Switch
       onChange={onChange}
